3631c8c009265c84999100e083d121bd5ecdd004
darpasoft <igorssac@gmail.com>
2020-07-21 18:09:23 +0100

    fix Eureka AMI info

--- src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
@@ -3,13 +3,31 @@ package com.virtualpairprogrammers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableScheduling
 public class FleetmanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FleetmanApplication.class, args);		
-	}	
+	}
+	
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
+	{
+		EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+		instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		instance.setDataCenterInfo(info);
+		instance.setNonSecurePort(8080);
+		return instance;
+	}
+	
 }
 
